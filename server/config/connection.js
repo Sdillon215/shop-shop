b21877608341883c07ce22c2606b62bb9ea6ceed
Sean <sdillon215@gmail.com>
2021-11-19 22:45:11 -0800

    ssf

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// mongodb shopping-cart
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shopping-cart', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
